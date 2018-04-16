c44be7b01a509051b4ef336bcd67494ecad73a7b
Boris Schmidt <schmidt.boris.bs@gmail.com>
2018-04-16 14:57:38 +0200

    Add documentation fetchAll

---
+++ examples/fetchAll.php
@@ -0,0 +1,18 @@
+<?php
+require __DIR__ . '/../vendor/autoload.php';
+
+use Fei\Service\Translate\Client\Translate;
+use Fei\ApiClient\Transport\BasicTransport;
+
+try {
+    $translate = new Translate([Translate::OPTION_BASEURL => 'http://translate.dev']);
+    $translate->setTransport(new BasicTransport());
+    $translate->fetchAll();
+} catch (\Exception $e) {
+    echo $e->getMessage() . PHP_EOL;
+    $previous = $e->getPrevious();
+    if ($previous instanceof Guzzle\Http\Exception\ServerErrorResponseException) {
+        var_dump($previous->getRequest());
+        var_dump($previous->getResponse()->getBody(true));
+    }
+}
